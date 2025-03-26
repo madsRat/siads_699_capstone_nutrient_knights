0dd0c7fd2005ccefffe04e351e4ce29abf6e1d36
madsRat <torrecam@umich.edu>
2025-03-26 10:51:08 -0400

    computed basal metabolic rate (bmr)

--- calculator_nutrient_needs.py
+++ calculator_nutrient_needs.py
@@ -18,24 +18,24 @@ def parse_amount_unit(value):
 def preprocess_anthropometrics():
 
     # feed in fake data
-    sex = 'male'
+    sex = 'Male'
     age = '30 years'  # years
     weight = '160 lbs'  # lbs
-    height = "5' 11" #'72 inches' # inches
-    activity_level = 'active'
+    height = "6'" #'72 inches' # inches
+    activity_level = 'Active'
 
     # unit conversions to metric for DRI calculations
     # target variables weight and height
     CONVERSIONS = {
         # cover all possible representations of pounds
-        "lb": (2.20462, "kg"),  # kilograms
-        "-lb": (2.20462, "kg"),  # kilograms
-        "lbs": (2.20462, "kg"),  # kilograms
-        "-lbs": (2.20462, "kg"),  # kilograms
-        "pound": (2.20462, "kg"),  # kilograms
-        "-pound": (2.20462, "kg"),  # kilograms
-        "pounds": (2.20462, "kg"),  # kilograms
-        "-pounds": (2.20462, "kg"),  # kilograms
+        "lb": (0.453592, "kg"),  # kilograms
+        "-lb": (0.453592, "kg"),  # kilograms
+        "lbs": (0.453592, "kg"),  # kilograms
+        "-lbs": (0.453592, "kg"),  # kilograms
+        "pound": (0.453592, "kg"),  # kilograms
+        "-pound": (0.453592, "kg"),  # kilograms
+        "pounds": (0.453592, "kg"),  # kilograms
+        "-pounds": (0.453592, "kg"),  # kilograms
 
         "kg": (1, "kg"),  # kilograms
         "g": (0.001, "kg"),  # kilograms
@@ -77,7 +77,11 @@ def preprocess_anthropometrics():
         if "'" in metric:
             height = metric.split("'")
             feet = int(height[0])
-            inches = int(height[1])
+
+            if len(height) > 2:
+                inches = int(height[1])
+            else:
+                inches = 0
 
             val = 2.54 * (12 * feet + inches) # in centimeters
             unit = 'cm'
@@ -91,39 +95,73 @@ def preprocess_anthropometrics():
             val = round(float(val) * factor, 2)
             unit = target_unit
 
-        # anthro_amount_units.append([val, unit])
-        anthropometrics[key] = ([val, unit])
+        alternative_names = {
+            'inactive': 'inactive',
+            'in-active': 'inactive',
+            'not active': 'inactive',
 
+            'low_active': 'low_active',
+            'low active': 'low_active',
+            'not very active': 'low_active',
+
+            'active': 'active',
+
+            'very_active': 'very_active',
+            'very active': 'very_active',
+
+            'male': 'male',
+            'm': 'male',
+            'female': 'female',
+            'f': 'female',
+        }
+
+        # format sex and activity_level
+        if key in ['activity_level', 'sex']:
+            val = val.lower()
+            val = alternative_names[val]
+
+        anthropometrics[key] = val  # [val, unit]
+
+    print(anthropometrics)
     return anthropometrics
 
-    # def calculate_patient_needs():
-
-    # # print('sex', sex)
-    # # print('age:', age)
-    # # print('weight:', weight)
-    # # print('height:', height)
-    # # print('activity_level:', activity_level)
-    # # print('\n')
-    # #
-    # # # import DRI Tables
-    # # if sex == 'male':
-    # #     df = pd.read_excel('DRI_TABLES.xlsx', sheet_name='male')
-    # # if sex == 'female':
-    # #     df = pd.read_excel('DRI_TABLES.xlsx', sheet_name='female')
-    # #
-    # # print('Loaded', sex, 'dataset.')
-    # # print(df)
-    # #
-    # # # Compute Basal Metabolic Rate
-
-
-def basal_metabolic_rate():
-    # Compute Basal Metabolic Rate
+def basal_metabolic_rate(patient_info, df):
+    # Compute Basal Metabolic Rate (BMR)
     activity_levels = {
         'inactive': 1.4,
-        'low active': 1.6,
+        'low_active': 1.6,
         'active': 1.75,
-        'very active': 2.0,
+        'very_active': 2.0,
     }
 
-preprocess_anthropometrics()
+    if patient_info['sex'] == 'male':
+        s_constant = 5
+    elif patient_info['sex'] == 'female':
+        s_constant = -161
+    else:
+        s_constant = None
+        print("Patient Sex undefined.")
+
+    BMR = 10 * patient_info['weight'] + 6.25 * patient_info['height'] - 5 * patient_info['age'] + s_constant
+    bmr_adjusted = BMR * activity_levels[patient_info['activity_level']]
+
+    return round(bmr_adjusted, 2)
+
+def calculate_patient_needs(patient_info):
+
+    # import DRI Tables
+    if patient_info['sex'] == 'male':
+        df = pd.read_excel('DRI_TABLES.xlsx', sheet_name='male')
+    if patient_info['sex']== 'female':
+        df = pd.read_excel('DRI_TABLES.xlsx', sheet_name='female')
+
+    print('Loaded', patient_info['sex'], 'dataset.')
+    print(df)
+
+    # Compute Basal Metabolic Rate
+    bmr = basal_metabolic_rate(patient_info, df)
+    print('Basal Metabolic Rate:', bmr)
+
+patient_info = preprocess_anthropometrics()
+calculate_patient_needs(patient_info)
+
